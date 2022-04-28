ea5de3c52bcf668bc46c358a99d5ed74431eac63
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2022-04-28 19:45:14 +0530

    Update songs.py

--- YukkiMusic/plugins/tools/songs.py
+++ YukkiMusic/plugins/tools/songs.py
@@ -34,26 +34,55 @@ SONG_COMMAND = get_command("SONG_COMMAND")
 )
 @language
 async def song_commad_private(client, message: Message, _):
-    await message.delete()
-    url = await YouTube.url(message)
-    if url:
-        if not await YouTube.exists(url):
-            return await message.reply_text(_["song_5"])
+    try:
+        await message.delete()
+        url = await YouTube.url(message)
+        if url:
+            if not await YouTube.exists(url):
+                return await message.reply_text(_["song_5"])
+            mystic = await message.reply_text(_["play_1"])
+            (
+                title,
+                duration_min,
+                duration_sec,
+                thumbnail,
+                vidid,
+            ) = await YouTube.details(url)
+            if str(duration_min) == "None":
+                return await mystic.edit_text(_["song_3"])
+            if int(duration_sec) > SONG_DOWNLOAD_DURATION_LIMIT:
+                return await mystic.edit_text(
+                    _["play_4"].format(
+                        SONG_DOWNLOAD_DURATION, duration_min
+                    )
+                )
+            buttons = song_markup(_, vidid)
+            await mystic.delete()
+            return await message.reply_photo(
+                thumbnail,
+                caption=_["song_4"].format(title),
+                reply_markup=InlineKeyboardMarkup(buttons),
+            )
+        else:
+            if len(message.command) < 2:
+                return await message.reply_text(_["song_2"])
         mystic = await message.reply_text(_["play_1"])
-        (
-            title,
-            duration_min,
-            duration_sec,
-            thumbnail,
-            vidid,
-        ) = await YouTube.details(url)
+        query = message.text.split(None, 1)[1]
+        try:
+            (
+                title,
+                duration_min,
+                duration_sec,
+                thumbnail,
+                vidid,
+            ) = await YouTube.details(query)
+        except:
+            return await mystic.edit_text(_["play_3"])
         if str(duration_min) == "None":
             return await mystic.edit_text(_["song_3"])
         if int(duration_sec) > SONG_DOWNLOAD_DURATION_LIMIT:
             return await mystic.edit_text(
-                _["play_4"].format(
-                    SONG_DOWNLOAD_DURATION, duration_min
-                )
+                _["play_6"].format(SONG_DOWNLOAD_DURATION, duration_min)
             )
         buttons = song_markup(_, vidid)
         await mystic.delete()
@@ -62,34 +91,8 @@ async def song_commad_private(client, message: Message, _):
             caption=_["song_4"].format(title),
             reply_markup=InlineKeyboardMarkup(buttons),
         )
-    else:
-        if len(message.command) < 2:
-            return await message.reply_text(_["song_2"])
-    mystic = await message.reply_text(_["play_1"])
-    query = message.text.split(None, 1)[1]
-    try:
-        (
-            title,
-            duration_min,
-            duration_sec,
-            thumbnail,
-            vidid,
-        ) = await YouTube.details(query)
     except:
-        return await mystic.edit_text(_["play_3"])
-    if str(duration_min) == "None":
-        return await mystic.edit_text(_["song_3"])
-    if int(duration_sec) > SONG_DOWNLOAD_DURATION_LIMIT:
-        return await mystic.edit_text(
-            _["play_6"].format(SONG_DOWNLOAD_DURATION, duration_min)
-        )
-    buttons = song_markup(_, vidid)
-    await mystic.delete()
-    return await message.reply_photo(
-        thumbnail,
-        caption=_["song_4"].format(title),
-        reply_markup=InlineKeyboardMarkup(buttons),
-    )
+        pass
 
 
 @app.on_callback_query(
