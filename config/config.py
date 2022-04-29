e42e4423ef9da5dbf0194283cb20c5ed09d8e9d1
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2022-04-29 12:14:04 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -68,7 +68,7 @@ SUPPORT_CHANNEL = "https://t.me/TechZBots"
 SUPPORT_GROUP = "https://t.me/TechZBots_Support"
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
-AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", None)
+AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False").capitalize()
 
 # Time after which you're assistant account will leave chats automatically.
 AUTO_LEAVE_ASSISTANT_TIME = int(
