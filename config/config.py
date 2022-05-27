0bc923a3c3502a8606dffab31c7d838d69f8245c
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2022-05-27 21:20:30 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -24,7 +24,9 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 # Database to save your chats and stats... Get MongoDB:-  https://telegra.ph/How-To-get-Mongodb-URI-04-06
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+MONGO_DB_URI = getenv("MONGO_DB_URI", None).strip()
+if MONGO_DB_URI == "":
+    MONGO_DB_URI = None
 
 # Custom max audio(music) duration for voice chat. set DURATION_LIMIT in variables with your own time(mins), Default to 60 mins.
 DURATION_LIMIT_MIN = int(
