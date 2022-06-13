e7acb4ad772fb493c072c6c621f6b41fd52c1475
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2022-06-13 21:35:10 +0530

    Update assistant.py

--- YukkiMusic/plugins/techzbots/assistant.py
+++ YukkiMusic/plugins/techzbots/assistant.py
@@ -1,5 +1,22 @@
 from pyrogram import Client, filters
 from YukkiMusic.misc import SUDO_USERS as SUDOERS
+from YukkiMusic.core.userbot import assistants
+from YukkiMusic.utils.database import get_client
+
+if 1 in assistants:
+    ASS1 = await get_client(1)
+
+if 2 in assistants:
+    ASS2 = await get_client(2)
+
+if 3 in assistants:
+    ASS3 = await get_client(3)
+
+if 4 in assistants:
+    ASS4 = await get_client(4)
+
+if 5 in assistants:
+    ASS5 = await get_client(5)
 
 @Client.on_message(
     filters.private
