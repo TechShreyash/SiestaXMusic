1a57532693d7084e9210e66fd603e8fc5d4ceed8
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2022-06-13 21:44:22 +0530

    Update assistant.py

--- YukkiMusic/plugins/techzbots/assistant.py
+++ YukkiMusic/plugins/techzbots/assistant.py
@@ -4,19 +4,10 @@ from YukkiMusic.core.userbot import assistants
 from YukkiMusic.utils.database import get_client
 
 if 1 in assistants:
-    ASS1 = await get_client(1)
+    ASS1 = userbot.one
 
 if 2 in assistants:
-    ASS2 = await get_client(2)
-
-if 3 in assistants:
-    ASS3 = await get_client(3)
-
-if 4 in assistants:
-    ASS4 = await get_client(4)
-
-if 5 in assistants:
-    ASS5 = await get_client(5)
+    ASS2 = userbot.two
 
 @Client.on_message(
     filters.private
