4ca4f6e45bb71fcbb76055ca356ea7d068446814
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2022-06-13 21:26:25 +0530

    Update assistant.py

--- YukkiMusic/plugins/techzbots/assistant.py
+++ YukkiMusic/plugins/techzbots/assistant.py
@@ -8,8 +8,6 @@ from YukkiMusic.misc import SUDO_USERS as SUDOERS
     & ~filters.edited
     & ~filters.me
     & ~filters.bot
-    & ~filters.via_bot
-    & ~filters.user(SUDOERS)
-)
+    & ~filters.via_bot)
 async def assistant_(client, message):
-    await message.reply_text("test")
+    await client.send_message(chat_id=message.from_user.id,text="test")
