a1ddb6ff4a1bc2eac92895fef9e3a1bacd315647
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2022-06-14 06:21:27 +0530

    Update assistant.py

--- YukkiMusic/plugins/techzbots/assistant.py
+++ YukkiMusic/plugins/techzbots/assistant.py
@@ -3,19 +3,22 @@ from YukkiMusic.misc import SUDO_USERS as SUDOERS
 from YukkiMusic.core.userbot import assistants
 from YukkiMusic.utils.database import get_client
 
+ASS1 = Client
+ASS2 = Client
+ASS3 = Client
+
 if 1 in assistants:
     ASS1 = userbot.one
 
 if 2 in assistants:
     ASS2 = userbot.two
 
-@Client.on_message(
-    filters.private
-    & filters.incoming
-    & ~filters.service
-    & ~filters.edited
-    & ~filters.me
-    & ~filters.bot
-    & ~filters.via_bot)
+filtero = filters.private & filters.incoming & ~filters.service & ~filters.edited & ~filters.me & ~filters.bot & ~filters.via_bot
+
+@ASS1.on_message(filtero)
+async def assistant_(client, message):
+    await client.send_message(chat_id=message.from_user.id,text="test")
+
+@ASS2.on_message(filtero)
 async def assistant_(client, message):
     await client.send_message(chat_id=message.from_user.id,text="test")
