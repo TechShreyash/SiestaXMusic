400a95cb0fbcf763fd49bfe204d6db6e81571853
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2022-06-17 15:58:21 +0530

    Update assistant.py

--- YukkiMusic/plugins/techzbots/assistant.py
+++ YukkiMusic/plugins/techzbots/assistant.py
@@ -10,8 +10,6 @@ ASS2 = userbot.two
 
 filtero = filters.private & filters.incoming & ~filters.service & ~filters.edited & ~filters.me & ~filters.bot & ~filters.via_bot
 
-@ASS1.on_message(filtero)
-@ASS2.on_message(filtero)
-@Client.on_message(filters.incoming)
+@Client.on_message(filtero)
 async def assistant_(client, message):
-    await client.send_message(chat_id=message.from_user.id,text="test")
+    await message.reply_text("hi")
